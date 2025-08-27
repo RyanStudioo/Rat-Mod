f3b6828db915c5371c7b57f65777c943b4dee420
RyanStudioo <yanyan2019ry@gmail.com>
2025-08-27 16:01:40 +0800

    readded cheese and cheese_blocks for 1.21.4
    added loot tables and block modifiers for cheese_blocks

---
+++ src/main/java/io/github/ryanstudioo/items/ModItems.java
@@ -0,0 +1,45 @@
+package io.github.ryanstudioo.items;
+
+import io.github.ryanstudioo.Ratmod;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.component.type.FoodComponent;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
+
+import java.util.function.Function;
+
+public class ModItems {
+    public static Item cheese;
+
+    public static Item register(String name, Function<Item.Settings, Item> itemFactory, Item.Settings settings) {
+        // Create the item key.
+        RegistryKey<Item> itemKey = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(Ratmod.MOD_ID, name));
+
+        // Create the item instance.
+        Item item = itemFactory.apply(settings.registryKey(itemKey));
+
+        // Register the item.
+        Registry.register(Registries.ITEM, itemKey, item);
+
+        return item;
+    }
+
+    public static void addToItemGroup(RegistryKey<ItemGroup> group, Item item){
+        ItemGroupEvents.modifyEntriesEvent(group).register((itemGroup) -> itemGroup.add(item));
+    }
+
+    public static void initialize() {
+        cheese = register("cheese", Item::new, new Item.Settings().food(new FoodComponent.Builder()
+                .nutrition(4)
+                .saturationModifier(1)
+                .build()));
+        addToItemGroup(ItemGroups.FOOD_AND_DRINK, cheese);
+    }
+
+}
