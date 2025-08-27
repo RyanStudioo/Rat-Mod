caba5668dacd4a5ce77548a7b4167b2345e09869
RyanStudioo <150920961+RyanStudioo@users.noreply.github.com>
2025-08-27 22:48:16 +0800

    Rat Model functional

--- src/client/java/io/github/ryanstudioo/RatmodClient.java
+++ src/client/java/io/github/ryanstudioo/RatmodClient.java
@@ -1,10 +1,20 @@
 package io.github.ryanstudioo;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.rendering.v1.EntityModelLayerRegistry;
+import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
+import net.minecraft.client.render.entity.model.SilverfishEntityModel;
 
 public class RatmodClient implements ClientModInitializer {
-	@Override
-	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
-	}
+    @Override
+    public void onInitializeClient() {
+        // Register the model layer with silverfish model data
+        EntityModelLayerRegistry.registerModelLayer(
+                RatEntityModel.LAYER,
+                SilverfishEntityModel::getTexturedModelData
+        );
+
+        // Register the entity renderer
+        EntityRendererRegistry.register(ModEntities.RAT, EntityRenderer::new);
+    }
 }
